955e2e894973cc3b5051cc34860adb945ed3d141
Boris D <boris.dob@gmail.com>
2017-08-10 10:19:32 -0700

    Add comments.

--- src/Page/Counter.jsx
+++ src/Page/Counter.jsx
@@ -2,26 +2,38 @@ import React from 'react'
 import { connect } from 'nuclear-js-react-addons'
 import { inc, dec } from '../util'
 
+// Action types
+
 export const Increment = 'Counter_Increment'
 export const Decrement = 'Counter_Decrement'
+
+// Action creators
 
 export const actions = {
   increment (reactor) { reactor.dispatch(Increment) },
   decrement (reactor) { reactor.dispatch(Decrement) },
 }
 
+// Model
+
 export const initialState = {
   count: 0,
 }
+
+// Update
 
 export const reducers = [
   [Increment, (state) => { return state.update('count', inc) }],
   [Decrement, (state) => { return state.update('count', dec) }],
 ]
 
+// Store queries
+
 export const getters = {
   count: ['CounterStore', 'count'],
 }
+
+// View
 
 class CounterPage extends React.Component {
   render () {
